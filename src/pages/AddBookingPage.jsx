fb9c14d610ff70e6b41213d75055e9117e50f02f
Coder Sourya <thecreativeworldforall@gmail.com>
2022-07-06 21:44:45 +0530

    Creating UI Pages

---
+++ src/pages/AddBookingPage.jsx
@@ -0,0 +1,4 @@
+const AddBookingPage = () => {
+    return (<h4>Add Booking Page</h4>)
+}
+export default AddBookingPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+const HomePage = (props) => {
+    console.log(props);
+    return (
+        <h6>Home Page</h6>
+    )
+}
+export default HomePage
