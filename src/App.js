5f6535f2d62f05c64a8dd984124b309d9cde6266
Coder Sourya <thecreativeworldforall@gmail.com>
2022-07-06 21:47:01 +0530

    Modifying Router Configuration and App Basic Styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import './App.css';
-import {Redirect, Route,Switch} from 'react-router-dom'
+import { Redirect, Route, Switch, useParams, useLocation } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import AddBookingPage from './pages/AddBookingPage';
+import Header from './components/Header/Header';
 
 function App() {
+
+  let info = useParams();
+  const location = useLocation();
+  console.log(info);
+  console.log(location);
   return (
     <div className="App">
-       <p className='text-center m-0 bg-black'>Advance Medico</p>
-       <Switch>
-          <Route path="/" exact>
-            <Redirect to="/home"></Redirect>
-          </Route>
-          <Route path="/home" exact>
-            {<p>Home</p>}
-          </Route>
-          <Route path="/add">
-            {<p>Add Patient</p>}
-          </Route>
-          <Route path="*">
-            {<p>ERROR</p>}
-          </Route>
-       </Switch>
+      <Header />
+
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home"></Redirect>
+        </Route>
+        <Route path="/home" >
+          <HomePage />
+        </Route>
+        <Route path="/book-appointment">
+          <AddBookingPage />
+        </Route>
+        <Route path="/bookings" exact>
+          {<p>Bookings</p>}
+        </Route>
+        <Route path="/booking/:bookingId">
+          {<p>Booking ID</p>}
+        </Route>
+        <Route path="/booking/:bookingId/edit">
+          {<p>Edit Booking ID</p>}
+        </Route>
+        <Route path="/user">
+          {<p>User</p>}
+        </Route>
+        <Route path="*">
+          {<p>ERROR</p>}
+        </Route>
+      </Switch>
     </div>
   );
 }
