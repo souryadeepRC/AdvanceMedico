bf5a7b71cdf48053bf69155672d6dd953d98906c
Coder Sourya <thecreativeworldforall@gmail.com>
2022-07-16 22:22:50 +0530

    Adding Test classes in Header Component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,10 @@ import classes from "./Header.module.css";
 const Header = () => {
   return (
     <header className={classes.header__section}>
-        <p className={classes.header__icon}>Icon</p>
+      <p className={classes.header__icon}>Icon</p>
       <p className={classes.header__text}>Advance Medico</p>
-       
-        <NavigationLink />
-       
+
+      <NavigationLink />
     </header>
   );
 };
