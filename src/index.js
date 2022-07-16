8572dcdc1abcbad573f462511aa6c6cde2fad327
Coder Sourya <thecreativeworldforall@gmail.com>
2022-07-16 22:23:48 +0530

    modifying index file and Home Component

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter><App /></BrowserRouter>
-  );
+root.render(<BrowserRouter><App /></BrowserRouter>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
-const HomePage = (props) => {
-    console.log(props);
+const HomePage = (props) => { 
     return (
         <h6>Home Page</h6>
     )
